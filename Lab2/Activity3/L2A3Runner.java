edac98adbd4256a16e02ee5bfe1b5c9deaaece18
ShehrozeEhsan086 <80197565+ShehrozeEhsan086@users.noreply.github.com>
2021-03-08 00:11:37 +0500

    Update L2A3Runner.java

--- Lab2/Activity3/L2A3Runner.java
+++ Lab2/Activity3/L2A3Runner.java
@@ -3,10 +3,12 @@ package l2a3;
 import javax.swing.JOptionPane;
 
 public class L2A3Runner {
-    CarPart carOne = new CarPart();
-    String x = JOptionPane.showInputDialog("What is Model Number?" );
-    String y = JOptionPane.showInputDialog("What is Part Number?" );
-    String z = JOptionPane.showInputDialog("What is Cost?" );
-    carOne.setparameters(x,y,z);
-    carOne.display();
+    public static void main(String[] args){
+        CarPart carOne = new CarPart();
+        String x = JOptionPane.showInputDialog("What is Model Number?" );
+        String y = JOptionPane.showInputDialog("What is Part Number?" );
+        String z = JOptionPane.showInputDialog("What is Cost?" );
+        carOne.setparameters(x,y,z);
+        carOne.display();
+    }
 }
