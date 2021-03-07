bab087303018f2d4d7d9e83f836b24eb9668fe7d
ShehrozeEhsan086 <80197565+ShehrozeEhsan086@users.noreply.github.com>
2021-03-07 15:17:07 +0500

    Add files via upload

---
+++ Lab2/Activity1/CourseResult.java
@@ -0,0 +1,14 @@
+
+package l2a1;
+
+
+public class CourseResult {
+    public String studentname;
+    public String coursename;
+    public String grade;
+    
+public void display()
+{
+    System.out.println("Student Name is: " + studentname + "\nCourse Name is: " + coursename + "\nGrade is: " + grade);
+}
+}
---
+++ Lab2/Activity1/L2A1Runner.java
@@ -0,0 +1,20 @@
+
+package l2a1;
+
+
+public class L2A1Runner {
+
+   
+    public static void main(String[] args) {
+        CourseResult c1=new CourseResult ();
+        c1.studentname= "Ali";
+        c1.coursename= "OOP";
+        c1.grade= "A";
+        c1.display();
+        CourseResult c2=new CourseResult ();
+        c2.studentname= "Saba";
+        c2.coursename= "ICP";
+        c2.grade= "A+";
+c2.display();
+    }
+}
