3d1ce023544f75c7ce5ab184b59aaf838a374360
ShehrozeEhsan086 <80197565+ShehrozeEhsan086@users.noreply.github.com>
2021-03-08 10:03:30 +0500

    Add files via upload

---
+++ Lab4/Activity3/Lab4_Activity3_Runner.java
@@ -0,0 +1,14 @@
+package lab4_;
+
+public class Lab4_Activity3_Runner {
+    public static void main (String[] args){
+        
+    Point p1 = new Point();
+    p1.setX(10);
+    p1.setY(7);
+    p1.display();
+    Point p2 = new Point(10,11);
+    p2.movePoint(2,3);
+    p2.display();
+}
+}
