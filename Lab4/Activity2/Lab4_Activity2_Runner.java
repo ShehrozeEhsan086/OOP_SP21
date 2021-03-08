8d5146db7ff64b89219fcc496e3016d3c92e13df
ShehrozeEhsan086 <80197565+ShehrozeEhsan086@users.noreply.github.com>
2021-03-08 09:50:54 +0500

    Add files via upload

---
+++ Lab4/Activity2/Lab4_Activity2_Runner.java
@@ -0,0 +1,13 @@
+package lab4_;
+
+public class Lab4_Activity2_Runner {
+    public static void main (String[] args){
+        Rectangle rect = new Rectangle();
+        rect.setLength(5);
+        rect.setWidth(10);
+        System.out.println("Area of Rectangle is " + rect.area( ));
+        System.out.println("width of Rectangle is " + rect.getWidth());
+
+    }
+
+}
---
+++ Lab4/Activity2/Rectangle.java
@@ -0,0 +1,39 @@
+package lab4_;
+
+public class Rectangle {
+    
+    private int length, width;
+
+    public Rectangle(){
+
+        length = 5; width = 2;
+    }
+
+    public Rectangle(int l, int w){
+   
+        length = l; width = w;
+    }
+
+    public void setLength(int l) {
+
+        length = l;
+    }
+
+    public void setWidth(int w) {
+        width = w;
+    }
+    
+    public int getLength(){
+        return length;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public int area (){
+        return (length*width);
+    }
+}
+
+
