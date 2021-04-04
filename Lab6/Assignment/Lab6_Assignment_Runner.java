91c2c1015054b0057fb8e4db31a7a9f0a8e38ee1
ShehrozeEhsan086 <80197565+ShehrozeEhsan086@users.noreply.github.com>
2021-04-04 20:54:38 +0500

    Add files via upload

---
+++ Lab6/Assignment/Lab6_Assignment_Runner.java
@@ -0,0 +1,15 @@
+package lab6_assignment;
+
+public class Lab6_Assignment_Runner {
+    public static void main(String[] args) {
+
+        System.out.println("1 + 2 = "+Calculator.sum(1,2));
+        System.out.println("1 x 2 = "+Calculator.multiply(1,2));
+        System.out.println("1 / 2 = "+Calculator.divide(1,2));
+        System.out.println("1 modulus 2 = "+Calculator.modulus(1,2));
+        System.out.println("Cos 1 = "+Calculator.cos(1));
+        System.out.println("Sin 2 = "+Calculator.sin(2));
+        System.out.println("Tan 3 = "+Calculator.tan(3));
+
+    }
+}
