788d3e6fb7431985da993d59846e6111faacfb6d
ShehrozeEhsan086 <80197565+ShehrozeEhsan086@users.noreply.github.com>
2021-04-29 05:08:10 +0500

    Delete Lab9 directory

--- Lab9/Activity1/Lab9_Activity1_Runner.java
+++
@@ -1,8 +0,0 @@
-package lab9_activity1;
-
-public class Lab9_Activity1_Runner {
-    public static void main(String args[]){
-        B subOb = new B(1, 2, 3);
-        subOb.show(); // this calls show() in B
-    }
-}
